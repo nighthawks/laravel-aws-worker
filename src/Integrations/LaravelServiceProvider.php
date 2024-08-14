8eeac045af16172582f2d9d5669226a130fc8624
Svetlin Stoev <svetliostoev@gmail.com>
2024-08-14 18:46:30 +0300

    Extend base QueueServiceProvider.php

--- src/Integrations/LaravelServiceProvider.php
+++ src/Integrations/LaravelServiceProvider.php
@@ -10,12 +10,13 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Queue\Events\JobProcessed;
 use Illuminate\Queue\QueueManager;
+use Illuminate\Queue\QueueServiceProvider;
 
 /**
  * Class CustomQueueServiceProvider
  * @package App\Providers
  */
-class LaravelServiceProvider extends ServiceProvider
+class LaravelServiceProvider extends QueueServiceProvider
 {
     use BindsWorker;
 
@@ -28,6 +29,8 @@ class LaravelServiceProvider extends ServiceProvider
 
         $this->bindWorker();
         $this->addRoutes();
+
+        parent::register();
     }
 
     /**
@@ -39,13 +42,13 @@ class LaravelServiceProvider extends ServiceProvider
         $this->app['router']->post('/worker/queue', 'Dusterio\AwsWorker\Controllers\WorkerController@queue');
     }
 
-    /**
-     * @return void
-     */
-    public function boot()
-    {
-        $this->app->singleton(QueueManager::class, function() {
-            return new QueueManager($this->app);
-        });
-    }
+    // /**
+    //  * @return void
+    //  */
+    // public function boot()
+    // {
+    //     $this->app->singleton(QueueManager::class, function () {
+    //         return new QueueManager($this->app);
+    //     });
+    // }
 }

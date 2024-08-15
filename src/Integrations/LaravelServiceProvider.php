8847260d2ed1857e93076a8502032b50351d0832
Svetlin Stoev <svetliostoev@gmail.com>
2024-08-15 10:24:38 +0300

    Update LaravelServiceProvider.php

--- src/Integrations/LaravelServiceProvider.php
+++ src/Integrations/LaravelServiceProvider.php
@@ -6,6 +6,12 @@ use Dusterio\AwsWorker\Wrappers\DefaultWorker;
 use Dusterio\AwsWorker\Wrappers\Laravel53Worker;
 use Dusterio\AwsWorker\Wrappers\WorkerInterface;
 use Dusterio\PlainSqs\Sqs\Connector;
+use Illuminate\Queue\Connectors\BeanstalkdConnector;
+use Illuminate\Queue\Connectors\DatabaseConnector;
+use Illuminate\Queue\Connectors\NullConnector;
+use Illuminate\Queue\Connectors\RedisConnector;
+use Illuminate\Queue\Connectors\SqsConnector;
+use Illuminate\Queue\Connectors\SyncConnector;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Queue\Events\JobProcessed;
@@ -26,7 +32,7 @@ class LaravelServiceProvider extends ServiceProvider
     {
         if (function_exists('env') && ! env('REGISTER_WORKER_ROUTES', true)) return;
 
-        // $this->bindWorker();
+        $this->bindWorker();
         $this->addRoutes();
     }
 
@@ -42,10 +48,106 @@ class LaravelServiceProvider extends ServiceProvider
     /**
      * @return void
      */
-    // public function boot()
-    // {
-    //     $this->app->singleton(QueueManager::class, function () {
-    //         return new QueueManager($this->app);
-    //     });
-    // }
+    public function boot()
+    {
+        $this->app->singleton(QueueManager::class, function ($app) {
+            // Once we have an instance of the queue manager, we will register the various
+            // resolvers for the queue connectors. These connectors are responsible for
+            // creating the classes that accept queue configs and instantiate queues.
+            return tap(new QueueManager($app), function ($manager) {
+                $this->registerConnectors($manager);
+            });
+        });
+    }
+
+    /**
+     * Register the connectors on the queue manager.
+     *
+     * @param  \Illuminate\Queue\QueueManager  $manager
+     * @return void
+     */
+    public function registerConnectors($manager)
+    {
+        foreach (['Null', 'Sync', 'Database', 'Redis', 'Beanstalkd', 'Sqs'] as $connector) {
+            $this->{"register{$connector}Connector"}($manager);
+        }
+    }
+
+    /**
+     * Register the Null queue connector.
+     *
+     * @param  \Illuminate\Queue\QueueManager  $manager
+     * @return void
+     */
+    protected function registerNullConnector($manager)
+    {
+        $manager->addConnector('null', function () {
+            return new NullConnector;
+        });
+    }
+
+    /**
+     * Register the Sync queue connector.
+     *
+     * @param  \Illuminate\Queue\QueueManager  $manager
+     * @return void
+     */
+    protected function registerSyncConnector($manager)
+    {
+        $manager->addConnector('sync', function () {
+            return new SyncConnector;
+        });
+    }
+
+    /**
+     * Register the database queue connector.
+     *
+     * @param  \Illuminate\Queue\QueueManager  $manager
+     * @return void
+     */
+    protected function registerDatabaseConnector($manager)
+    {
+        $manager->addConnector('database', function () {
+            return new DatabaseConnector($this->app['db']);
+        });
+    }
+
+    /**
+     * Register the Redis queue connector.
+     *
+     * @param  \Illuminate\Queue\QueueManager  $manager
+     * @return void
+     */
+    protected function registerRedisConnector($manager)
+    {
+        $manager->addConnector('redis', function () {
+            return new RedisConnector($this->app['redis']);
+        });
+    }
+
+    /**
+     * Register the Beanstalkd queue connector.
+     *
+     * @param  \Illuminate\Queue\QueueManager  $manager
+     * @return void
+     */
+    protected function registerBeanstalkdConnector($manager)
+    {
+        $manager->addConnector('beanstalkd', function () {
+            return new BeanstalkdConnector;
+        });
+    }
+
+    /**
+     * Register the Amazon SQS queue connector.
+     *
+     * @param  \Illuminate\Queue\QueueManager  $manager
+     * @return void
+     */
+    protected function registerSqsConnector($manager)
+    {
+        $manager->addConnector('sqs', function () {
+            return new SqsConnector;
+        });
+    }
 }
